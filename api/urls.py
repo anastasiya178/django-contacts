df3be5484eff2878782ad30d85c74047fc03cbce
akrasnokutskaya <anastasiia.lipatova@gmail.com>
2021-10-18 15:34:53 +0300

    amended Api tests and Contacts tests

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ app_name = "api"
 urlpatterns = [
     path('contacts/', ContactList.as_view(), name="api_contacts_list"),
     path('contacts/<int:pk>', ContactDetail.as_view(), name="api_contact_detail"),
+    path('delete/<int:pk>', ContactDetail.as_view(), name="api_contact_delete")
 ]
--- contacts/tests.py
+++ contacts/tests.py
@@ -8,32 +8,21 @@ from django.urls import reverse
 from .models import Contact
 
 
-# # creating a new contact
-# class ContactModelTestCase(TestCase):
-#     def setUp(self):
-#         Contact.objects.create(name="Lion", email="lion@myemail.com")
-#         Contact.objects.create(id="444", name="Lion", email="lion@myemail.com")
-#
-#     def test_get_absolute_url(self):
-#         pass
-
-
-# unauth user can't access the website
 class GetHomePageTestCase(TestCase):
 
-    def test_anonymous_cannot_see_page(self):
+    def test_anonymous_get_page(self):
         """anonymous user can't access the website and gets redirected to Login page"""
         response = self.client.get(reverse("contacts:index"))
         self.assertRedirects(response, "/accounts/login/")
 
-    def test_authenticated_user_can_see_page(self):
+    def test_authenticated_user_get_page(self):
         """logged-in user can access the page"""
         user = User.objects.create_user("Katy", "katy_pass")
         self.client.force_login(user=user)
         response = self.client.get(reverse("contacts:index"))
         self.assertEqual(response.status_code, 200)
 
-    def test_post_contact(self):
+    def test_create_contact(self):
         """logged-in user with required permissions can create a new contact instance"""
         user = User.objects.create_user("Katy", "katy_pass")
         editor_group = Group.objects.create(name='Editor')
@@ -42,6 +31,21 @@ class GetHomePageTestCase(TestCase):
         self.client.force_login(user=user)
         data = {"name": "Joe K", "email": "joe.s@gmail.com"}
         response = self.client.post(reverse("contacts:create_contact"), data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("contacts:index"))
+
+    def test_delete_contact(self):
+        """logged-in user with required permissions can delete a contact instance"""
+        user = User.objects.create_user("Katy_admin", "katy_pass")
+        editor_group = Group.objects.create(name='Admin')
+        editor_group.permissions.set([25, 26, 27, 28])
+        editor_group.user_set.add(user)
+        self.client.force_login(user=user)
+        contact = Contact(name="Harry G", email="hat@mail.com")
+        contact.save()
+        pk = contact.pk
+        url = reverse("contacts:delete_contact", args=[pk])
+        response = self.client.post(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("contacts:index"))
 
