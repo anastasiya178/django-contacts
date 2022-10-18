394be3ebf2884721116777f4af01b196a2a0a7cd
anastasiakr <anastasiia.lipatova@gmail.com>
2022-10-18 17:07:03 -0500

    [feat] Updated Contacts unit tests

--- contacts/tests.py
+++ contacts/tests.py
@@ -1,6 +1,6 @@
 """Unit tests for Contacts app"""
+import unittest
 
-from django.http import response
 from django.test import TestCase
 from django.contrib.auth.models import User, Group
 from django.urls import reverse
@@ -8,46 +8,101 @@ from django.urls import reverse
 from .models import Contact
 
 
-class GetHomePageTestCase(TestCase):
-
+class GetHomePageAnonymousTestCase(TestCase):
+    @unittest.skip("In development")
     def test_anonymous_get_page(self):
-        """anonymous user can't access the website and gets redirected to Login page"""
+        """Assert that anonymous user can't access the web page and gets redirected to Login page"""
         response = self.client.get(reverse("contacts:index"))
         self.assertRedirects(response, "/accounts/login/")
 
+
+class GetHomePageAuthenticatedTestCase(TestCase):
+    def setUp(self) -> None:
+        """"Set up a user, log them in. Create a new contact"""
+        self.user = User.objects.create_user("Katy", "katy_pass")
+        self.client.force_login(user=self.user)
+
+        self.contact = Contact(first_name="Harry", last_name="Jones", email="hatj@mail.com")
+        self.contact.save()
+
     def test_authenticated_user_get_page(self):
-        """logged-in user can access the page"""
-        user = User.objects.create_user("Katy", "katy_pass")
-        self.client.force_login(user=user)
+        """Assert that authenticated user with no permissions assigned can access the web page"""
         response = self.client.get(reverse("contacts:index"))
         self.assertEqual(response.status_code, 200)
 
+
+class GetHomePageAdminTestCase(TestCase):
+    def setUp(self) -> None:
+        """"Set up a user, log them in. Create a new contact"""
+        self.user = User.objects.create_user("Katy", "katy_pass")
+        editor_group = Group.objects.create(name='Admin')
+        editor_group.permissions.set([25, 26, 27, 28])
+        editor_group.user_set.add(self.user)
+        self.client.force_login(user=self.user)
+
+        self.contact = Contact(first_name="Harry", last_name="Jones", email="hatj@mail.com")
+        self.contact.save()
+
+    def test_admin_view_index_page(self):
+        """Assert that authenticated Admin user can access the web page and gets redirected to Login page"""
+        response = self.client.get(reverse("contacts:index"))
+        self.assertEqual(response.status_code, 200)
+
+    @unittest.skip("In development")
     def test_create_contact(self):
-        """logged-in user with required permissions can create a new contact instance"""
-        user = User.objects.create_user("Katy", "katy_pass")
-        editor_group = Group.objects.create(name='Editor')
-        editor_group.permissions.set([25, 28])
-        editor_group.user_set.add(user)
-        self.client.force_login(user=user)
-        data = {"name": "Joe K", "email": "joe.s@gmail.com"}
-        response = self.client.post(reverse("contacts:create_contact"), data)
+        """Assert that authenticated user with Editor permissions can create a new contact"""
+        new_contact_data = {'first_name': 'Iris', 'last_name': 'Jones', 'email': 'irisj@mygmail.com'}
+        response = self.client.post(reverse("contacts:create_contact"), new_contact_data)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("contacts:index"))
 
+    def test_edit_contact(self):
+        """Assert that authenticated user with Admin permissions can edit a contact
+        TBD
+        """
+        pass
+
     def test_delete_contact(self):
-        """logged-in user with required permissions can delete a contact instance"""
-        user = User.objects.create_user("Katy_admin", "katy_pass")
-        editor_group = Group.objects.create(name='Admin')
-        editor_group.permissions.set([25, 26, 27, 28])
-        editor_group.user_set.add(user)
-        self.client.force_login(user=user)
-        contact = Contact(name="Harry G", email="hat@mail.com")
-        contact.save()
-        pk = contact.pk
-        url = reverse("contacts:delete_contact", args=[pk])
+        """Assert that authenticated user with Admin permissions can delete a contact instance"""
+        url = reverse("contacts:delete_contact", args=[self.contact.pk])
         response = self.client.post(url)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("contacts:index"))
 
 
+class GetHomePageEditorTestCase(TestCase):
+    def setUp(self) -> None:
+        """"Set up a user, log them in. Create a new contact"""
+        self.user = User.objects.create_user("Katy", "katy_pass")
+        editor_group = Group.objects.create(name='Editor')
+        editor_group.permissions.set([25, 28])
+        editor_group.user_set.add(self.user)
+        self.client.force_login(user=self.user)
 
+        self.contact = Contact(first_name="Harry", last_name="Jones", email="hatj@mail.com")
+        self.contact.save()
+
+    def test_view_index_page(self):
+        """Assert that authenticated Editor user can access the index page"""
+        response = self.client.get(reverse("contacts:index"))
+        self.assertEqual(response.status_code, 200)
+
+    @unittest.skip("In development")
+    def test_create_contact(self):
+        """Assert that authenticated user with Editor permissions can create a new contact"""
+        new_contact_data = {'first_name': 'Iris', 'last_name': 'Jones', 'email': 'irisj@mygmail.com'}
+        response = self.client.post(reverse("contacts:create_contact"), new_contact_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("contacts:index"))
+
+    def test_edit_contact(self):
+        """Assert that authenticated user with Editor permissions can edit a new contact
+        TBD
+        """
+        pass
+
+    def test_delete_contact(self):
+        """Assert that authenticated user with Editor permissions cannot delete a contact instance"""
+        url = reverse("contacts:delete_contact", args=[self.contact.pk])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 403)
