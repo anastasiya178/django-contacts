d8d76519b2e2e5068090df7c4268545224cfa8b4
anastasiakr <anastasiia.lipatova@gmail.com>
2024-09-02 18:29:13 -0400

    [feat] Added filter to contact_list.html. Adjusted ContactList view in views. Imported django-filters.

--- contacts/models.py
+++ contacts/models.py
@@ -1,13 +1,9 @@
 """Models for Contact app"""
-
-
 from django.db import models
 from django.urls import reverse
 
 
 # Create your models here.
-
-
 class Contact(models.Model):
     id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=255, default=None)
