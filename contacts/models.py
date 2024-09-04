0989822fbed185da266ff82f2f4a2c51e9ee74eb
anastasiakr <anastasiia.lipatova@gmail.com>
2024-09-03 20:54:29 -0400

    [feat] Introduced new models: Animal, Pet, Breed. Registered the new models in admin.py. Added basic fixtures.

--- contacts/models.py
+++ contacts/models.py
@@ -15,3 +15,40 @@ class Contact(models.Model):
 
     def get_absolute_url(self):
         return reverse('contacts:index')
+
+
+class Pet(models.Model):
+    MALE_OR_FEMALE = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("U", "Unknown"),
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    gender = models.CharField(choices=MALE_OR_FEMALE, default="Unknown")
+    dob = models.DateField()
+    color = models.CharField(max_length=255, default=None)
+    owner = models.ForeignKey("Contact", on_delete=models.CASCADE)
+    animal = models.ForeignKey("Animal", on_delete=models.CASCADE)
+    breed = models.ForeignKey("Breed", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}, {self.animal}"
+
+
+class Animal(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Breed(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    animal = models.ForeignKey("Animal", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
